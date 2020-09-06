76b7db1d7b5a5ce3b69764691d3fe5b8fe67e537
Jorge Obiols <jorge.obiols@gmail.com>
2020-09-06 04:25:53 -0300

    [DOC] fix tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,14 +4,14 @@
 
 # restaurar la base de test vacia
 cp /odoo_ar/odoo-11.0/backup_test/default_test.zip /odoo_ar/odoo-11.0/test11/backup_dir/
-oe --restore -d tatakua_test -c tatakua -f tatakua_test.zip
+oe --restore -d tatakua_test -c tatakua -f test11_test.zip
 
 # correr los tests
 sudo docker run --rm -it \
     -v /odoo_ar/odoo-11.0/tatakua/config:/opt/odoo/etc/ \
     -v /odoo_ar/odoo-11.0/tatakua/data_dir:/opt/odoo/data \
     -v /odoo_ar/odoo-11.0/tatakua/sources:/opt/odoo/custom-addons \
-    --link pg-tatakua:db \
+    --link pg-test11:db \
     jobiols/odoo-ent:11.0 -- \
        -i  odoo_uml \
    --stop-after-init -d tatakua_test --test-enable
